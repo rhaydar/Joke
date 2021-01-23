38aad304f19a81cda93465748c8768c34c79b81e
Rashedul Haydar <rh2712@columbia.edu>
2021-01-22 23:39:02 -0500

    Ch. 18: Show joke in a notification.
    The joke instead of being shown in the log, is now shown in a notification.
    The version of NotificationCompat.Builder(Context) is now deprecated, and
    instead must use NotificationCompat.Builder(Context, String), where the
    second argument is a channel ID. The link to this channel setup code is
    in the code.

--- app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -1,12 +1,20 @@
 package com.hfad.joke;
 
 import android.app.IntentService;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
+    public static final int NOTIFICATION_ID = 5453;
+    public static final String CHANNEL_ID = "joke";
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -25,7 +33,45 @@ public class DelayedMessageService extends IntentService {
         }
     }
 
-    private void showText(final String message) {
-        Log.v("DelayedMessageService", "The message is: " + message);
+    private void showText(final String text) {
+        /* Create a notification channel before building the notification. */
+        createNotificationChannel();
+
+        /* Create a notification builder. */
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle(getString(R.string.question))
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[] {0, 1000})
+                .setAutoCancel(true);
+
+        /* Create an action. */
+        Intent actionIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingActionIntent = PendingIntent.getActivity(this, 0,
+                actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingActionIntent);
+
+        /* Issue the notification. */
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    /* From https://developer.android.com/training/notify-user/build-notification#java */
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
